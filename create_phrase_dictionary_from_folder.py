2f42d60e2873fb2556af0213a2904f34389bea51
Brent Payne <brent@server1.motif.io>
2013-11-17 11:21:51 -0800

    upping default phrase generation numbers

--- create_phrase_dictionary_from_folder.py
+++ create_phrase_dictionary_from_folder.py
@@ -10,11 +10,11 @@ def main(folder, pkl_filename, options={}):
     corpus.add_folder(folder)
     pd = generate_phrases(
         corpus
-        , word_filter_num=1
-        , phrase_filter_num=2
-        , total_number_of_phrases=10 if 'phrase_count' not in options else int(options['phrase_count'])
-        , colloc_num_per_round=3 if 'colloc_per_round' not in options else int(options['colloc_per_round'])
-        , colloc_rounds=4
+        , word_filter_num=10
+        , phrase_filter_num=5
+        , total_number_of_phrases=1000 if 'phrase_count' not in options else int(options['phrase_count'])
+        , colloc_num_per_round=200 if 'colloc_per_round' not in options else int(options['colloc_per_round'])
+        , colloc_rounds=5
     )
     with open(pkl_filename, 'w') as fp:
         pickle.dump(pd, fp)
