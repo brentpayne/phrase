e90e5ee7c2857f10ee5efe78c5ad5902df7522c4
Brent Payne <brentpayne@Brents-MacBook-Air.local>
2013-11-17 10:43:04 -0800

    script fix

--- create_phrase_dictionary_from_folder.py
+++ create_phrase_dictionary_from_folder.py
@@ -5,15 +5,15 @@ from phrase_generation import generate_phrases
 __author__ = 'brentpayne'
 
 
-def main(folder, pkl_filename):
+def main(folder, pkl_filename, options={}):
     corpus = FileCorpus()
     corpus.add_folder(folder)
     pd = generate_phrases(
         corpus
         , word_filter_num=1
         , phrase_filter_num=2
-        , total_number_of_phrases=10 if 'phrase_count' not in data else int(data['phrase_count'])
-        , colloc_num_per_round=3 if 'colloc_per_round' not in data else int(data['colloc_per_round'])
+        , total_number_of_phrases=10 if 'phrase_count' not in options else int(options['phrase_count'])
+        , colloc_num_per_round=3 if 'colloc_per_round' not in options else int(options['colloc_per_round'])
         , colloc_rounds=4
     )
     with open(pkl_filename) as fp:
